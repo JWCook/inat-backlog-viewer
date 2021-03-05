e948658f7cdbeb0173d659cf86e7db810bf2a4e7
Jordan Cook <jordan.cook@pioneer.com>
2021-03-05 16:35:28 -0600

    Followed tutorial at https://travishorn.com/creating-a-photo-gallery-with-vue-css-grid-3e0a3dd25285

--- src/router/index.js
+++ src/router/index.js
@@ -1,30 +1,19 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
-
-Vue.use(VueRouter);
-
-const routes = [
-  {
-    path: "/",
-    name: "Home",
-    component: Home
-  },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
-  }
-];
-
-const router = new VueRouter({
-  mode: "history",
+import Vue from 'vue';
+import Router from 'vue-router';
+import Home from '../views/Home.vue';
+import Photo from '../views/Photo.vue';Vue.use(Router);export default new Router({
+  mode: 'history',
   base: process.env.BASE_URL,
-  routes
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home,
+    },
+    {
+      path: '/photo/:id',
+      name: 'photo',
+      component: Photo,
+    },
+  ],
 });
-
-export default router;
